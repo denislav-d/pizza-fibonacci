28f2345e52d99d3ff74ce38dc044661e97881e4f
denislav-d <ddenislav333@gmail.com>
2024-02-26 17:34:55 +0100

    Initial Commit

---
+++ PizzaFibonacci/PizzaFibonacciApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaFibonacciApp.swift
+//  PizzaFibonacci
+//
+//  Created by Denislav Dimitrov on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct PizzaFibonacciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
